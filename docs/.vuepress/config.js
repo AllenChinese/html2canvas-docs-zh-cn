b5bb07a3c17d943c5b09915a9fc9d6e123257182
AllenChinese <13402202685@163.com>
2019-09-17 15:03:24 +0800

    feat: Add Proxy document page

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -32,7 +32,8 @@ module.exports = {
       ['/docs/html2canvas-about', '关于'],
       ['/docs/html2canvas-getStart.md', '入门'],
       ['/docs/html2canvas-configuration.md', '配置项'],
-      ['/docs/html2canvas-features.md', '功能']
+      ['/docs/html2canvas-features.md', '功能'],
+      ['/docs/html2canvas-proxy.md', '代理']
     ]
   }
 }
